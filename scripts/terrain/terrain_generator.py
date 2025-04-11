151689d7aeb143952086f3cddd3c5a1fb74ecba7
faffonso <faffonso@usp.br>
2025-04-10 22:39:03 -0300

    terrain scripts

--- scripts/terrain/terrain_generator.py
+++ scripts/terrain/terrain_generator.py
@@ -1,10 +1,5 @@
-# SPDX-License-Identifier: BSD-3-Clause
-
-"""Script to visualize only the ROUGH terrain defined in ROUGH_TERRAINS_CFG."""
-
 from isaaclab.app import AppLauncher
 
-# inicia a simulação
 app_launcher = AppLauncher()
 simulation_app = app_launcher.app
 
@@ -15,40 +10,28 @@ from isaaclab.utils import configclass
 from isaaclab.terrains import TerrainImporterCfg
 from isaaclab.utils.assets import ISAACLAB_NUCLEUS_DIR
 
-from terrain_generator import WAVE_ONLY_CFG
+from terrain_generator_cfg import MULTIPLE_TERRAINS_CFG, UNIQUE_TERRAINS_CFG
 
 @configclass
 class TerrainSceneCfg(InteractiveSceneCfg):
-    """Cena com apenas o terreno rough gerado."""
-
     terrain = TerrainImporterCfg(
         prim_path="/World/ground",
         terrain_type="generator",
-        terrain_generator=WAVE_ONLY_CFG,
+        terrain_generator=UNIQUE_TERRAINS_CFG,
         max_init_terrain_level=5,
         collision_group=-1,
         physics_material=sim_utils.RigidBodyMaterialCfg(
             friction_combine_mode="multiply",
             restitution_combine_mode="multiply",
             static_friction=1.0,
-            dynamic_friction=1.0,
-        ),
-        visual_material=sim_utils.MdlFileCfg(
-            mdl_path=f"{ISAACLAB_NUCLEUS_DIR}/Materials/TilesMarbleSpiderWhiteBrickBondHoned/TilesMarbleSpiderWhiteBrickBondHoned.mdl",
-            project_uvw=True,
-            texture_scale=(0.25, 0.25),
+            dynamic_friction=2.0,
         ),
         debug_vis=True,
     )
 
-    sky_light = AssetBaseCfg(
-        prim_path="/World/skyLight",
-        spawn=sim_utils.DomeLightCfg(
-            intensity=1000.0,
-            texture_file=f"{ISAACLAB_NUCLEUS_DIR}/Materials/Textures/Skies/PolyHaven/kloofendal_43d_clear_puresky_4k.hdr",
-        ),
+    dome_light = AssetBaseCfg(
+        prim_path="/World/Light", spawn=sim_utils.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75))
     )
-
 
 def main():
     sim_cfg = sim_utils.SimulationCfg(device="cuda:0")
@@ -59,7 +42,7 @@ def main():
     scene = InteractiveScene(scene_cfg)
 
     sim.reset()
-    print("[INFO] Terreno rough gerado. Pressione ESC para fechar.")
+    print("[INFO] Terreno gerado. Pressione ESC para fechar.")
 
     while simulation_app.is_running():
         sim.step()
--- scripts/terrain/terrain_generator_cfg.py
+++ scripts/terrain/terrain_generator_cfg.py
@@ -1,17 +1,59 @@
-from omni.isaac.lab.terrains import TerrainGeneratorCfg, HfWaveTerrainCfg
+import isaaclab.terrains as terrain_gen
 
-WAVE_ONLY_CFG = TerrainGeneratorCfg(
+MULTIPLE_TERRAINS_CFG = terrain_gen.TerrainGeneratorCfg(
     size=(8.0, 8.0),
     border_width=10.0,
-    num_rows=1,
-    num_cols=1,
+    num_rows=4,
+    num_cols=4,
     horizontal_scale=0.1,
     vertical_scale=0.005,
     slope_threshold=0.75,
     use_cache=False,
     sub_terrains={
-        "wave_terrain": HfWaveTerrainCfg(
-            proportion=1.0, amplitude_range=(0.1, 0.2), num_waves=4, border_width=0.25
+        "wave_terrain": terrain_gen.HfWaveTerrainCfg(
+            proportion=0.25, 
+            amplitude_range=(0.0, 0.2), 
+            num_waves=8, 
+            border_width=0.25
+        ),
+	    "stepping_stone_terrain": terrain_gen.HfSteppingStonesTerrainCfg(
+            proportion=0.25,
+            stone_height_max=0.05,                  
+            stone_width_range=(0.2, 0.4),            
+            stone_distance_range=(0.03, 0.08),         
+            border_width=0.25                        
+        ),
+	    "gravel_terrain": terrain_gen.HfRandomUniformTerrainCfg(
+	        proportion=0.25,                         
+    		noise_range=(0.02, 0.05),              
+    		noise_step=0.01,                       
+    		border_width=0.25                      
+	    ),
+
+		"rails_terrain": terrain_gen.MeshRailsTerrainCfg(
+            proportion=0.25,
+            rail_thickness_range=(0.05, 0.1),  
+            rail_height_range=(0.05, 0.15),    
+            platform_width=1.0
+        )
+    },
+)
+
+UNIQUE_TERRAINS_CFG = terrain_gen.TerrainGeneratorCfg(
+    size = (8.0, 8.0),
+    border_width = 10.0,
+    num_rows = 4,
+    num_cols = 4,
+    horizontal_scale = 0.1,
+    vertical_scale = 0.005,
+    slope_threshold = 0.75,
+    use_cache = False,
+    sub_terrains = {
+        "wave_terrain": terrain_gen.HfWaveTerrainCfg(
+            proportion = 1.0, 
+            amplitude_range = (0.0, 0.1), 
+            num_waves = 8, 
+            border_width = 0.25
         )
     },
 )
